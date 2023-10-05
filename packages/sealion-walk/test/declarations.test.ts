3bc1068ca4ed1e7a01426aec3630d1eca2715a99
wfalcon0x <wfalcon0x808@gmail.com>
2023-10-05 22:28:21 +0100

    add function declaration test

--- packages/sealion-walk/test/declarations.test.ts
+++ packages/sealion-walk/test/declarations.test.ts
@@ -1,6 +1,7 @@
 import { simple } from '../src'
 
 import pragmaDeclaration from './fixtures/declarations/pragmaDeclaration.json'
+import functionDeclaration from './fixtures/declarations/functionDeclaration.json'
 
 describe('simple walker', () => {
   it('should walk pragma declaration', async () => {
@@ -13,6 +14,15 @@ describe('simple walker', () => {
       },
     }
     simple(pragmaDeclaration, visitor)
+  })
+
+  it('should walk function declaration', async () => {
+    const visitor = {
+      FunctionDeclaration: function (node) {
+        expect(node.Type).toBe('FunctionDeclaration')
+      },
+    }
+    simple(functionDeclaration, visitor)
   })
 
 })
